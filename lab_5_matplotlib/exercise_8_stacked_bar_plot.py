8ff7c7e5ba8f48a8cd4b51580ec987e5d5f2efb1
Shigeo Kitamura <shigeo9462@gmail.com>
2024-10-10 16:33:14 -0700

    fix stacked bar plot

--- lab_5_matplotlib/exercise_8_stacked_bar_plot.py
+++ lab_5_matplotlib/exercise_8_stacked_bar_plot.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 # Data
 categories = ["Group 1", "Group 2", "Group 3"]
@@ -7,9 +9,18 @@ value2 = [6, 8, 4]
 value3 = [4, 3, 5]
 
 # Plotting
-plt.stackplot(categories, value1, value2, value3)
-plt.legend(["Value 1", "Value 2", "Value 3"], loc="upper right")
+width = 0.35
+x = np.arange(len(categories))
+
+plt.bar(x, value1, width, label='Value 1')
+plt.bar(x, value2, width, bottom=value1, label='Value 2')
+plt.bar(x, value3, width, bottom=np.array(value1) + np.array(value2), label='Value 3')
+
 plt.xlabel("Categories")
 plt.ylabel("Values")
 plt.title("Stacked Bar Plot")
+plt.xticks(x, categories)
+plt.legend(loc="upper right")
+
+plt.tight_layout()
 plt.show()
